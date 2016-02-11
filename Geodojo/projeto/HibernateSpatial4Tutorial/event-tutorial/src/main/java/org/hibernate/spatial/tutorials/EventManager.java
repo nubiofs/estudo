3a73f7787257a86e6bfb937ccb1709a9ae35b359
nubiofs <nubiofs@gmail.com>
2016-02-11 11:50:46 -0300

    add

---
+++ Geodojo/projeto/HibernateSpatial4Tutorial/event-tutorial/src/main/java/org/hibernate/spatial/tutorials/EventManager.java
@@ -0,0 +1,67 @@
+package org.hibernate.spatial.tutorials;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import org.hibernate.spatial.tutorials.util.JPAUtil;
+import org.hibernate.spatial.tutorials.entity.Event;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+
+public class EventManager {
+	
+	//$ mvn exec:java -Dexec.mainClass="org.hibernate.spatial.tutorials.EventManager" -Dexec.args="store POINT(10 5)"
+	public static void main(String[] args) {
+        EventManager mgr = new EventManager();
+
+        if (args[0].equals("store")) {
+            mgr.createAndStoreEvent("My Event", new Date(), assemble(args));
+        }
+        JPAUtil.close();
+    }
+
+    private void createAndStoreEvent(String title, Date theDate, String wktPoint) {
+        Geometry geom = wktToGeometry(wktPoint);
+
+        if (!geom.getGeometryType().equals("Point")) {
+            throw new RuntimeException("Geometry must be a point. Got a " + geom.getGeometryType());
+        }
+
+        EntityManager em = JPAUtil.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Event theEvent = new Event();
+        theEvent.setTitle(title);
+        theEvent.setDate(theDate);
+        theEvent.setLocation((Point) geom);
+        em.persist(theEvent);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    private Geometry wktToGeometry(String wktPoint) {
+        WKTReader fromText = new WKTReader();
+        Geometry geom = null;
+        try {
+            geom = fromText.read(wktPoint);
+        } catch (ParseException e) {
+            throw new RuntimeException("Not a WKT string:" + wktPoint);
+        }
+        return geom;
+    }
+
+    /**
+     * Utility method to assemble all arguments save the first into a String
+     */
+    private static String assemble(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < args.length; i++) {
+            builder.append(args[i]).append(" ");
+        }
+        return builder.toString();
+    }
+
+}
