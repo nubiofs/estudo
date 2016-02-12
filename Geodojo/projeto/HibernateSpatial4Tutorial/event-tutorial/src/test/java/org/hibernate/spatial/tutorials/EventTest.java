6ba0a02e915b15bb72452ba54c88695bafdd6281
nubiofs <nubiofs@gmail.com>
2016-02-12 12:02:36 -0300

    Novo método de teste

--- Geodojo/projeto/HibernateSpatial4Tutorial/event-tutorial/src/test/java/org/hibernate/spatial/tutorials/EventTest.java
+++ Geodojo/projeto/HibernateSpatial4Tutorial/event-tutorial/src/test/java/org/hibernate/spatial/tutorials/EventTest.java
@@ -2,45 +2,80 @@ package org.hibernate.spatial.tutorials;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import org.hibernate.spatial.tutorials.entity.Event;
 import org.hibernate.spatial.tutorials.util.GeometryUtil;
 import org.hibernate.spatial.tutorials.util.JPAUtil;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
 
 public class EventTest {
 
-	EntityManager em;
+	static EntityManager em;
+	static Query SQL_GET_POINT;
+	String NOVO_PONTO = "POINT(55 77)";
 	
-	@Before
-	public void before() {
+	@BeforeClass
+	public static void beforeClass() {
 		em = JPAUtil.createEntityManager();
 		em.getTransaction().begin();
+		SQL_GET_POINT = em.createQuery(
+				"select e from Event e where st_within(e.location, :geo) = true", Event.class);
 	}
 	
-	@After
-	public void after() {
+	@AfterClass
+	public static void after() {
 		JPAUtil.close();
 	}
 	
 	@Test
 	public void getPoint() {
 
-		Query query = em.createQuery(
-				"select e from Event e where st_within(e.location, :geo) = true", Event.class);
-		
 		Geometry geo = GeometryUtil.wktToGeometry("POINT(10 5)");
-		query.setParameter("geo", geo);
+		SQL_GET_POINT.setParameter("geo", geo);
 		
-		Event event = (Event) query.getResultList().get(0);
+		Event event = (Event) SQL_GET_POINT.getResultList().get(0);
 
 		assertEquals("My Event", event.getTitle());
+		
+	}
+	
+	@Test
+	public void crudNovoPonto() {
+		
+		//
+		//Inserção do Novo ponto
+		//
+		String tituloNovoPonto = "Novo Ponto";
+		Geometry ponto = GeometryUtil.wktToGeometry(NOVO_PONTO);
+
+        if (!ponto.getGeometryType().equals("Point")) {
+            throw new RuntimeException("Geometry must be a point. Got a " + ponto.getGeometryType());
+        }
+
+        Event theEvent = new Event();
+        theEvent.setTitle(tituloNovoPonto);
+        theEvent.setDate(new Date());
+        theEvent.setLocation((Point) ponto);
+        em.persist(theEvent);
+        em.getTransaction().commit();
+        
+		//
+		//Consulta do Novo ponto
+		//
+		SQL_GET_POINT.setParameter("geo", ponto);
+		
+		Event event = (Event) SQL_GET_POINT.getResultList().get(0);
+
+		assertEquals(tituloNovoPonto, event.getTitle());
 		
 	}
 
