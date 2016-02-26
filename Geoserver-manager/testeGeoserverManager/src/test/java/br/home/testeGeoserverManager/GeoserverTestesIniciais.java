e809a6d6864550c13965dcf079d625aa6ac6c89f
nubiofs <nubiofs@gmail.com>
2016-02-26 16:41:04 -0300

    novos testes ver...

--- Geoserver-manager/testeGeoserverManager/src/test/java/br/home/testeGeoserverManager/GeoserverTestesIniciais.java
+++ Geoserver-manager/testeGeoserverManager/src/test/java/br/home/testeGeoserverManager/GeoserverTestesIniciais.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.net.MalformedURLException;
+import java.util.Map;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 
 import it.geosolutions.geoserver.rest.GeoServerRESTReader;
 import it.geosolutions.geoserver.rest.decoder.RESTLayer;
+import it.geosolutions.geoserver.rest.encoder.feature.FeatureTypeAttribute;
 
 public class GeoserverTestesIniciais {
 
@@ -44,9 +46,33 @@ public class GeoserverTestesIniciais {
 	@Test
 	public void getLayerStates() {
 		String layer = "states";
-		RESTLayer states = reader.getLayer("topp", layer);
+		String workspace = "topp";
+		RESTLayer states = reader.getLayer(workspace, layer);
 		assertEquals(layer, states.getName());
 		LOGGER.info("exist states Layer OK...");
+//		RESTResource resource = reader.getResource(states);
+//		System.out.println(resource.getAbstract());
+//		System.out.println(resource.getStoreUrl());
+//		System.out.println(resource.getStoreName());
+//		System.out.println(reader.getFeatureTypes(workspace));
+		
+		//System.out.println(reader.getFeatureType(states).toString());
+		
+		//System.out.println(reader.getFeatureType(states).getAttributeList().toString());
+		//for(Map<FeatureTypeAttribute, String> lista : reader.getFeatureType(states).getAttributeList()){
+		for(int i = 0; i < reader.getFeatureType(states).getAttributeList().size(); i++){
+			Map<FeatureTypeAttribute, String> ftMap = reader.getFeatureType(states).getAttributeList().get(i);
+			for(int j=0; j < ftMap.size(); j++){
+				if(ftMap.get(j).equals(FeatureTypeAttribute.name)){
+					
+				}
+			}
+//			if(lista.get("name").equals("the_geom")){
+//				//{name=the_geom, binding=com.vividsolutions.jts.geom.MultiPolygon, nillable=true, minOccurs=0, length=null, maxOccurs=1}
+//			}
+			//System.out.println(lista.toString());
+		}
+		
 	}
 
 }
