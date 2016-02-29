ef1271e02d08bc746a5feda6fb29a2ea666b856a
nubiofs <nubiofs@gmail.com>
2016-02-29 17:43:35 -0300

    versão inicial

---
+++ Demoiselle/cadastro/src/main/java/lab/cadastro/entity/Pessoa.java
@@ -0,0 +1,93 @@
+package lab.cadastro.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+
+@Entity
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private Integer id;
+
+	@NotNull
+	@Column(length = 50)
+	private String nome;
+
+	@Email
+	@NotNull
+	@Column(length = 255)
+	private String email;
+
+	@Column(length = 15)
+	private String telefone;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj){
+			return true;
+		}
+		if (obj == null){
+			return false;
+		}
+		if (getClass() != obj.getClass()){
+			return false;
+		}
+		Pessoa other = (Pessoa) obj;
+		if (id == null) {
+			if (other.id != null){
+				return false;
+			}
+		} else if (!id.equals(other.id)){
+			return false;
+		}
+		return true;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}
---
+++ Demoiselle/cadastro/src/main/java/lab/cadastro/persistence/PessoaDAO.java
@@ -0,0 +1,28 @@
+package lab.cadastro.persistence;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.gov.frameworkdemoiselle.annotation.Name;
+import br.gov.frameworkdemoiselle.transaction.Transactional;
+import br.gov.frameworkdemoiselle.util.Beans;
+import lab.cadastro.entity.Pessoa;
+
+@Transactional
+public class PessoaDAO {
+
+	@Inject
+	@Name("cadastro-ds")
+	private EntityManager em;
+	
+	//Esse método estático "getInstance", retorna uma instância controlada 
+	//pelo CDI da classe PessoaDAO
+	public static PessoaDAO getInstance() {
+        return Beans.getReference(PessoaDAO.class);
+    }
+	
+	public void insert(Pessoa pessoa) {
+        em.persist(pessoa);
+    }
+	
+}
