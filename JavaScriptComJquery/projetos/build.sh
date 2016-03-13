86427b0346c2b2c97a3fa8af91b732819c502657
nubiofs <nubiofs@gmail.com>
2016-03-13 11:51:18 -0300

    update melhorias ....

--- JavaScriptComJquery/projetos/build.sh
+++ JavaScriptComJquery/projetos/build.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
-export MY_PROJECT=testes/
-rm -rf /var/www/html/$MY_PROJECT
-cp -R $MY_PROJECT /var/www/html/
+#
+#Build do(s) projeto(s) no servidor web apache2
+#
+export APACHE_PATH=/var/www/html
+
+export MY_PROJECT=$1
+echo $MY_PROJECT
+
+if [ -z $MY_PROJECT ]; then
+    echo "Entre com o nome do diretório do projeto"
+    exit
+elif [ $MY_PROJECT = "." ]; then
+    cp $APACHE_PATH/index.html .
+    rm -rf $APACHE_PATH/*
+    cp -R . $APACHE_PATH/
+    rm -rf index.html
+    rm -rf $APACHE_PATH/build.sh
+else
+    rm -rf $APACHE_PATH/$MY_PROJECT
+    cp -R $MY_PROJECT $APACHE_PATH/
+fi
