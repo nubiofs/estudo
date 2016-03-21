21d2328307c0ca89162af841edad002742bccea2
nubiofs <nubiofs@gmail.com>
2016-03-21 09:16:42 -0300

    Atualização para mais de um tipo de Servidor Web (Além do Apache2 agora também o tomcat)...

--- JavaScriptComJquery/projetos/build.sh
+++ JavaScriptComJquery/projetos/build.sh
@@ -1,22 +1,58 @@
 #!/bin/sh
-#
-#Build do(s) projeto(s) no servidor web apache2
-#
-export APACHE_PATH=/var/www/html
 
-export MY_PROJECT=$1
-echo $MY_PROJECT
+#Nome do Servidor web
+export SERVER=$1
+
+if [ -z $SERVER ]; then
+    echo "Entre com o nome do servidor web: [apache2 | tomcat]"
+    exit
+elif [ $SERVER = "apache2" ]; then
+	
+	#Caminho para o Build do(s) projeto(s) no servidor web apache2
+	export SERVER_PATH=/var/www/html
+
+elif [ $SERVER = "tomcat" ]; then
+
+	#Caminho para o Build do(s) projeto(s) no servidor web apache2
+	export SERVER_PATH=/opt/demoiselle/apache-tomcat-9.0.0.M3/webapps
+
+fi
+
+echo "Servidor web: " $SERVER
+
+#Nome do projeto
+export MY_PROJECT=$2
+
+echo "Nome do diretório do projeto: " $MY_PROJECT
 
 if [ -z $MY_PROJECT ]; then
     echo "Entre com o nome do diretório do projeto"
     exit
 elif [ $MY_PROJECT = "." ]; then
-    cp $APACHE_PATH/index.html .
-    rm -rf $APACHE_PATH/*
-    cp -R . $APACHE_PATH/
-    rm -rf index.html
-    rm -rf $APACHE_PATH/build.sh
+	
+	if [ $SERVER = "apache2" ]; then
+	
+		cp $SERVER_PATH/index.html .
+		rm -rf $SERVER_PATH/*
+		cp -R . $SERVER_PATH/
+		rm -rf index.html
+		rm -rf $SERVER_PATH/build.sh
+	
+	else
+		
+		cp -R . $SERVER_PATH/
+		rm -rf $SERVER_PATH/build.sh
+		
+	fi
+	
 else
-    rm -rf $APACHE_PATH/$MY_PROJECT
-    cp -R $MY_PROJECT $APACHE_PATH/
+
+    rm -rf $SERVER_PATH/$MY_PROJECT
+    cp -R $MY_PROJECT $SERVER_PATH/
+    
 fi
+
+
+
+
+
