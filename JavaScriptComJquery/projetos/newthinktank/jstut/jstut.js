38eaafba9a697152abb90c55ff00dec6bdaccbb4
Nubio Souza <nubiofs@gmail.com>
2016-03-26 11:07:28 -0300

    u

--- JavaScriptComJquery/projetos/newthinktank/jstut/jstut.js
+++ JavaScriptComJquery/projetos/newthinktank/jstut/jstut.js
@@ -1,6 +1,32 @@
+
 var yourName = prompt("What is your name?");
 
 if (yourName != null) {
+    var msg = "Hello " + yourName + " !";
     document.getElementById("sayHello")
-        .innerHTML = "Hello " + yourName + " !";
+        .innerHTML = msg;
 }
+
+//comando ok, pois "msg" não está em um escopo de função!
+console.log("consegui? " + msg);
+
+/*
+function onDocumentLoad() {
+
+    var yourName = prompt("What is your name?");
+
+    if (yourName != null) {
+        //Para o comando "console.log("consegui? " + msg);" 
+        //abaixo vai dar:
+        //"ReferenceError: msg is not defined""
+        var msg = "Hello " + yourName + " !";
+        document.getElementById("sayHello")
+            .innerHTML = msg;
+    }
+    
+}
+
+window.onload = onDocumentLoad;
+
+console.log("consegui? " + msg);
+*/
