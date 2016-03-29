86600eaa611e5edbc79d64b1256452680b4e4d62
nubiofs <nubiofs@gmail.com>
2016-03-29 17:30:55 -0300

    new

---
+++ JavaScriptComJquery/projetos/jsUnitTest/01/js/index.js
@@ -0,0 +1,19 @@
+function welcome() {
+
+    var userName = document.getElementById("userName").value;
+    var msg = "Welcome " + userName + "!";
+
+    /* It does not work in Firefox! The reason behind this problem 
+    is that the innerText property is not supported in Firefox.
+    */
+    if (document.getElementById("welcomeMessage").innerText === undefined) {
+        //
+        //Mas para esse tipo de problema pode-se utilizar o jQuery:
+        //
+        $("#welcomeMessage").text(msg);
+
+    } else {
+        document.getElementById("welcomeMessage").innerText = msg;
+    }
+
+}
