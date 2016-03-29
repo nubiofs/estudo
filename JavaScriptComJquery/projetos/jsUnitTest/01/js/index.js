1d9379320e4d6d36c0fbb06d35551e79928d84df
nubiofs <nubiofs@gmail.com>
2016-03-29 17:33:22 -0300

    u

--- JavaScriptComJquery/projetos/jsUnitTest/01/js/index.js
+++ JavaScriptComJquery/projetos/jsUnitTest/01/js/index.js
@@ -6,14 +6,16 @@ function welcome() {
     /* It does not work in Firefox! The reason behind this problem 
     is that the innerText property is not supported in Firefox.
     */
-    if (document.getElementById("welcomeMessage").innerText === undefined) {
-        //
-        //Mas para esse tipo de problema pode-se utilizar o jQuery:
-        //
-        $("#welcomeMessage").text(msg);
+    // if (document.getElementById("welcomeMessage").innerText === undefined) {
+    //     //
+    //     //Mas para esse tipo de problema pode-se utilizar o jQuery:
+    //     //
+    //     $("#welcomeMessage").text(msg);
 
-    } else {
-        document.getElementById("welcomeMessage").innerText = msg;
-    }
-
+    // } else {
+    //     document.getElementById("welcomeMessage").innerText = msg;
+    // }
+    
+    $("#welcomeMessage").text(msg);
+    
 }
