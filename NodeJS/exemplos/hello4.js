f79fc775eec50fca983cb45943851ed3234a3d73
nubiofs <nubiofs@gmail.com>
2016-03-30 16:19:17 -0300

    u

--- NodeJS/exemplos/hello4.js
+++ NodeJS/exemplos/hello4.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
+    console.log("Vai ler arquivo...");
     // A constante __dirname retorna o diretório raiz da aplicação.
     fs.readFile(__dirname + '/index.html', function(err, html) {
         response.writeHeader(200, { 'Content-Type': 'text/html' });
@@ -11,5 +12,5 @@ var server = http.createServer(function(request, response) {
 });
 
 server.listen(3000, function() {
-    console.log('Executando Site Pessoal');
+    console.log('Executando Site Pessoal...');
 });
