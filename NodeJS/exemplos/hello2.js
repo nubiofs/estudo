d2078c32df77eb729b0c82b3c2c2746587063aa9
nubiofs <nubiofs@gmail.com>
2016-03-30 09:01:12 -0300

    inicio estudo

---
+++ NodeJS/exemplos/hello2.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1337;
+
+http.createServer((req, res) => {
+    
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World2\n');
+  
+}).listen(port, hostname, () => {
+    
+  console.log(`Server running at http://${hostname}:${port}/`);
+  
+});
