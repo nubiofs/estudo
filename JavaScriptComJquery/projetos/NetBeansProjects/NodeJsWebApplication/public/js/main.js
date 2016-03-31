11b691b74141b9ecd098bcf71e48b6232578478e
nubiofs <nubiofs@gmail.com>
2016-03-31 17:32:55 -0300

    u

---
+++ JavaScriptComJquery/projetos/NetBeansProjects/NodeJsWebApplication/public/js/main.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function () {
+    
+    var div = $("#divId");
+    
+    var msg = div.text()+" oi!";
+    
+    $("#divId").text(msg);
+    
+    console.log(msg);
+    
+});
+
