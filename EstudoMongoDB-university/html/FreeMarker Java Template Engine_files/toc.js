78f10cd294702b6f0b88c226058ab7e29a74f944
nubiofs <nubiofs@gmail.com>
2017-03-25 09:44:51 -0300

    v1

---
+++ EstudoMongoDB-university/html/FreeMarker Java Template Engine_files/toc.js
@@ -0,0 +1,266 @@
+
+var toc = {
+"title": "Apache FreeMarker",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "What is Apache FreeMarker?",
+"url": "index.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Download / Maven",
+"url": "freemarkerdownload.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Documentation",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "Manual",
+"url": "docs/index.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Java API",
+"url": "docs/api/index.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Manual Chinese\n translation",
+"url": "https://sourceforge.net/projects/freemarker/files/chinese-manual/",
+"isFile": true,
+"children": [
+]
+}
+]
+}
+,
+{
+"title": "Tooling",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "Editor and IDE plugins",
+"url": "editors.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Online template\n tester",
+"url": "http://freemarker-online.kenshoo.com/",
+"isFile": true,
+"children": [
+]
+}
+]
+}
+,
+{
+"title": "Community",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "Report bugs",
+"url": "https://issues.apache.org/jira/browse/FREEMARKER/",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Bugs still in the old\n tracker",
+"url": "https://sourceforge.net/p/freemarker/bugs/",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Report security vulnerability",
+"url": "report-security-vulnerabilities.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Ask help on Stack\n Overflow",
+"url": "http://stackoverflow.com/questions/ask?tags=freemarker",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Get news on Twitter",
+"url": "https://twitter.com/freemarker",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Discuss on mailing lists",
+"url": "mailing-lists.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Who uses FreeMarker?",
+"url": "poweredBy.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Contributors wanted!",
+"url": "contribute.html",
+"isFile": true,
+"children": [
+{
+"title": "How do I contribute?",
+"url": "contribute.html#how-to-contribute",
+"isFile": false,
+"children": [
+]
+}
+,
+{
+"title": "What should I contribute?",
+"url": "contribute.html#what-to-contribute",
+"isFile": false,
+"children": [
+]
+}
+]
+}
+,
+{
+"title": "Committer how-to",
+"url": "committer-howto.html",
+"isFile": true,
+"children": [
+{
+"title": "Git commit policies",
+"url": "committer-howto.html#git-commit-policy",
+"isFile": false,
+"children": [
+]
+}
+,
+{
+"title": "Merging in pull requests from GitHub",
+"url": "committer-howto.html#merging-pull-request",
+"isFile": false,
+"children": [
+]
+}
+,
+{
+"title": "Closing GitHub pull requests without merging",
+"url": "committer-howto.html#close-pull-request-without-merging",
+"isFile": false,
+"children": [
+]
+}
+,
+{
+"title": "Making releases",
+"url": "committer-howto.html#making-releases",
+"isFile": false,
+"children": [
+]
+}
+,
+{
+"title": "Updating the FreeMarker home page",
+"url": "committer-howto.html#updating-homepage",
+"isFile": false,
+"children": [
+]
+}
+,
+{
+"title": "Updating Docgen",
+"url": "committer-howto.html#updating-docgen",
+"isFile": false,
+"children": [
+]
+}
+,
+{
+"title": "Regarding editing the Manual and the Site DocBook",
+"url": "committer-howto.html#edit-docbook",
+"isFile": false,
+"children": [
+]
+}
+,
+{
+"title": "Dealing with security vulnerabilities",
+"url": "committer-howto.html#handle-security-vulnerabilities",
+"isFile": false,
+"children": [
+]
+}
+]
+}
+]
+}
+,
+{
+"title": "Miscellaneous",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "Source code (Git)",
+"url": "sourcecode.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Project history",
+"url": "history.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Sourceforge.net project\n page",
+"url": "https://sourceforge.net/projects/freemarker/",
+"isFile": true,
+"children": [
+]
+}
+]
+}
+]
+}
+;
