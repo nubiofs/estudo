6c35ee64fba8a40534622cdf4c06c2054e0ea939
Nubio Souza <nubiofs@gmail.com>
2017-04-08 18:10:20 -0300

    u

--- JsonPostgreSQL&MongoDB/pg_nosql_benchmark-master/testes/temps/bash.sh
+++ JsonPostgreSQL&MongoDB/pg_nosql_benchmark-master/testes/temps/bash.sh
@@ -3,7 +3,37 @@
 ################################################################################
 # set require variables.
 ################################################################################
-UM=1
-DOIS=2
+DIRECTORY=$(dirname $0)
+BASENAME=$(basename $0)
 
-echo 'SOMA: ' $UM + $DOIS
+COLLECTION_NAME="json_tables"
+SAMPLEJSON="sample.json"
+
+################################################################################
+# set mongo variables.
+################################################################################
+MONGO="/usr/bin/mongo"
+MONGOIMPORT="/usr/bin/mongoimport"
+MONGOHOST="127.0.0.1"
+MONGOPORT="27017"
+MONGOUSER="mongo"
+MONGOPASSWORD="mongo"
+MONGODBNAME="benchmark"
+
+################################################################################
+# source library files
+################################################################################
+source ${DIRECTORY}/lib/mongo_func_lib.sh
+
+################################################################################
+# main function
+################################################################################
+
+mongodb_version=$(mongo_version "${MONGOHOST}"     \
+                                "${MONGOPORT}"     \
+                                "${MONGODBNAME}"   \
+                                "${MONGOUSER}"     \
+                                "${MONGOPASSWORD}"
+                  )
+                  
+process_log "MongoDB Version $mongodb_version"
