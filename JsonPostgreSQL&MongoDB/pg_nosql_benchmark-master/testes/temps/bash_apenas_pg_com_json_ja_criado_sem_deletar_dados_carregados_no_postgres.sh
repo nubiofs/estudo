58c334dc9da3fd92018b78041c95859f7c7bdafe
nubiofs <nubiofs@gmail.com>
2017-04-10 16:08:54 -0300

    u

--- JsonPostgreSQL&MongoDB/pg_nosql_benchmark-master/testes/temps/bash_apenas_pg_com_json_ja_criado_sem_deletar_dados_carregados_no_postgres.sh
+++ JsonPostgreSQL&MongoDB/pg_nosql_benchmark-master/testes/temps/bash_apenas_pg_com_json_ja_criado_sem_deletar_dados_carregados_no_postgres.sh
@@ -38,8 +38,6 @@ source ${DIRECTORY}/lib/pg_func_lib.sh
 declare -a json_rows=(10000000)
 
 declare -a pg_size_time
-declare -a pg_copy_time
-declare -a pg_inserts_time
 declare -a pg_select_time
 
 ################################################################################
@@ -57,37 +55,6 @@ process_log "PostgreSQL Version $pg_version"
 
 for (( indx=0 ; indx < ${#json_rows[@]} ; indx++ ))
 do
-
-	#droping database benchmark if exists.
-	remove_pg_db "${PGHOST}"     \
-                "${PGPORT}"     \
-                "${PGDATABASE}" \
-                "${PGUSER}"     \
-                "${PGPASSWORD}"
-	#creating database benchmark.
-	create_pg_db "${PGHOST}"     \
-                "${PGPORT}"     \
-                "${PGDATABASE}" \
-                "${PGUSER}"     \
-                "${PGPASSWORD}"
-
-	#creating json_tables collection in postgreSQL.
-	mk_pg_json_collection "${PGHOST}"     \
-                         "${PGPORT}"     \
-                         "${PGDATABASE}" \
-                         "${PGUSER}"     \
-                         "${PGPASSWORD}" \
-                         "${COLLECTION_NAME}"
-
-	#loading data in postgresql using sample.json.
-	pg_copy_time[${indx}]=$(pg_copy_benchmark  "${PGHOST}"          \
-                                              "${PGPORT}"          \
-                                              "${PGDATABASE}"      \
-                                              "${PGUSER}"          \
-                                              "${PGPASSWORD}"      \
-                                              "${COLLECTION_NAME}" \
-                                              "${SAMPLEJSON}"
-                          )
 
 	#creating index on postgreSQL collections.
 	pg_create_index_collection "${PGHOST}"     \
@@ -114,40 +81,11 @@ do
                                             "${COLLECTION_NAME}"
                           )
 
-	#preparing postgresql INSERTs.
-	pg_json_insert_maker "${COLLECTION_NAME}"    \
-                        "${json_rows[${indx}]}" \
-                        "${PG_INSERTS}"
-
-	#droping json object in postgresql.
-	delete_json_data "${PGHOST}"      \
-                    "${PGPORT}"      \
-                    "${PGDATABASE}"  \
-                    "${PGUSER}"      \
-                    "${PGPASSWORD}"  \
-                    "${COLLECTION_NAME}"
-
-	#inserting data in postgresql using sample_pg_inserts.json.
-	pg_inserts_time[${indx}]=$(pg_inserts_benchmark  "${PGHOST}"          \
-                                                    "${PGPORT}"          \
-                                                    "${PGDATABASE}"      \
-                                                    "${PGUSER}"          \
-                                                    "${PGPASSWORD}"      \
-                                                    "${COLLECTION_NAME}" \
-                                                    "${PG_INSERTS}"
-                              )
-
 done
 
 print_result "number of rows"     "${json_rows[@]}"
 #
 # De (ns) "Nanosegundos" para (milissegundos) basta dividir 1000000
 #
-print_result "PG COPY (ns)"       "${pg_copy_time[@]}"
 print_result "PG SELECT (ns)"     "${pg_select_time[@]}"
-print_result "PG INSERT (ns)"     "${pg_inserts_time[@]}"
 print_result "PG SIZE (bytes)"    "${pg_size_time[@]}"
-
-#rm -rf ${SAMPLEJSON}*
-#rm -rf ${PG_INSERTS}
-
