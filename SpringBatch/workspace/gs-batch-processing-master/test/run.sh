8a154ba00d4a8f9a53dc41088d2cb9c715cacd0f
nubiofs <nubiofs@gmail.com>
2017-04-17 14:03:46 -0300

    u

--- SpringBatch/workspace/gs-batch-processing-master/test/run.sh
+++ SpringBatch/workspace/gs-batch-processing-master/test/run.sh
@@ -1,25 +1,9 @@
 #!/bin/sh
 cd $(dirname $0)
 
-cd ../complete
-
-mvn clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
-rm -rf target
-
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
-rm -rf build
-
 cd ../initial
 
-mvn clean compile
+~/mvn clean compile
 ret=$?
 if [ $ret -ne 0 ]; then
   exit $ret
