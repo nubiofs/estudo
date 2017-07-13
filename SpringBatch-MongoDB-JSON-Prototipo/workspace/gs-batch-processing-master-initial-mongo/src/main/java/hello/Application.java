29d0bc81586f026a8dc3d248a34650bd960abf58
nubiofs <nubiofs@gmail.com>
2017-07-13 16:26:33 -0300

    up

--- SpringBatch-MongoDB-JSON-Prototipo/workspace/gs-batch-processing-master-initial-mongo/src/main/java/hello/Application.java
+++ SpringBatch-MongoDB-JSON-Prototipo/workspace/gs-batch-processing-master-initial-mongo/src/main/java/hello/Application.java
@@ -1,15 +1,33 @@
 package hello;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws Exception {
-    	
-    	//Spring Boot’s SpringApplication.run() method to launch an application.
-        SpringApplication.run(Application.class, args);
-    }
-    
+	@Bean
+	public DataSource dataSource(Environment environment) {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:test");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("sa");
+		//Acessar "http://localhost:8080/h2console/"
+		//e criar rodar comandos DDL (spring-batch-core-3.0.7.RELEASE):
+		//Executing SQL script from class path resource [org/springframework/batch/core/schema-h2.sql
+		return dataSource;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		//Spring Boot’s SpringApplication.run() method to launch an application.
+		SpringApplication.run(Application.class, args);
+	}
+
 }
