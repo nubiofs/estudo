83320afa6cadfec39218b60b70e44c13e0b93e7c
nubiofs <nubiofs@gmail.com>
2017-07-19 17:25:15 -0300

    u

--- SpringBatch-MongoDB-JSON-Prototipo/workspace/gs-batch-processing-master-initial-mongo/src/main/java/hello/batch/JsonCarroLineMapper.java
+++ SpringBatch-MongoDB-JSON-Prototipo/workspace/gs-batch-processing-master-initial-mongo/src/main/java/hello/batch/JsonCarroLineMapper.java
@@ -4,8 +4,16 @@ import hello.pojo.Carro;
 
 import java.util.Map;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.JsonLineMapper;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 
 /**
  * This line mapper converts a JSON string to a {@link Carro}. 
@@ -14,16 +22,36 @@ public class JsonCarroLineMapper implements LineMapper<Carro> {
 
 	//@Autowired private org.springframework.batch.item.file.mapping.JsonLineMapper delegate;
 	private JsonLineMapper delegate = new JsonLineMapper();
-	
+
+	private static Logger logger = LoggerFactory.getLogger(JsonCarroLineMapper.class);
+
 	@Override
 	public Carro mapLine(String line, int lineNumber) throws Exception {
-		
-		//JSON
-		Map<String, Object> carroAsMap = (Map<String, Object>) delegate.mapLine(line, lineNumber);
 
-		Carro carro = new Carro();
-		carro.setKm((String) carroAsMap.get("km"));
-		carro.setNome((String) carroAsMap.get("nome"));
+		Carro carro = null;
+
+		try {
+
+			new Gson().fromJson(line, Object.class);
+
+			JSONObject jsObject = new JSONObject(line);
+
+			//JSON
+			Map<String, Object> carroAsMap = (Map<String, Object>) delegate.mapLine(line, lineNumber);
+			carro = new Carro();
+			
+			if(jsObject.has("km") && jsObject.has("nome")){//Verificação campos obrigatorios
+				carro.setKm((String) carroAsMap.get("km"));
+				carro.setNome((String) carroAsMap.get("nome"));
+			}
+
+		} catch(JsonSyntaxException ex) {
+			logger.info(Thread.currentThread().getId() + " (lineNumber: " + lineNumber + ") -> Exception: " + ex.getMessage());
+			throw ex;
+		} catch(JSONException ex){
+			logger.info(Thread.currentThread().getId() + " (lineNumber: " + lineNumber + ") -> Exception: " + ex.getMessage());
+			throw ex;
+		}
 
 		return carro;
 
