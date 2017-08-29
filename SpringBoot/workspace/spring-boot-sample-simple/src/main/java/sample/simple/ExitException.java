c5cb0060c8c042267f63a72549cc5b772adb2baf
nubiofs <nubiofs@gmail.com>
2017-08-29 16:14:04 -0300

    u

--- SpringBoot/workspace/spring-boot-sample-simple/src/main/java/sample/simple/ExitException.java
+++ SpringBoot/workspace/spring-boot-sample-simple/src/main/java/sample/simple/ExitException.java
@@ -20,6 +20,11 @@ import org.springframework.boot.ExitCodeGenerator;
 
 public class ExitException extends RuntimeException implements ExitCodeGenerator {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public int getExitCode() {
 		return 10;
