04d6b0b652e6115882c2d7a989dc142f17cfe0ba
nubiofs <nubiofs@gmail.com>
2018-12-06 10:56:33 -0300

    up

--- CienciasDosDados/pos-ver/atividade_final.r
+++ CienciasDosDados/pos-ver/atividade_final.r
@@ -15,26 +15,25 @@ base_aleatoria_2 = rnorm(70, 7, 1.5)
 (mean(base_aleatoria_2))
 
 # Ou via, exemplo extra:
-funcao_media = function(base_aleatoria) {
-  
-  soma = lapply(list(base_aleatoria), sum)
-  media = as.numeric(soma) / length(base_aleatoria)
-  
-  if (media ==  mean(base_aleatoria)) {#Se é igual ao resultado de 'mean'
-    
-    return(media)
-    
-  }
-  else {
-    
-    return("Error!")
-    
-  }
-  
-}
-
-(funcao_media(base_aleatoria_1))
-(funcao_media(base_aleatoria_2))
+# funcao_media = function(base_aleatoria) {
+#   
+#   soma = lapply(list(base_aleatoria), sum)
+#   media = as.numeric(soma) / length(base_aleatoria)
+#   
+#   if (media ==  mean(base_aleatoria)) {#Se é igual ao resultado de 'mean'
+#     
+#     return(media)
+#     
+#   }
+#   else {
+#     
+#     return("Error!")
+#     
+#   }
+#   
+# }
+# (funcao_media(base_aleatoria_1))
+# (funcao_media(base_aleatoria_2))
 
 # 2.b) Variância
 (var(base_aleatoria_1))
@@ -48,11 +47,13 @@ funcao_media = function(base_aleatoria) {
 (median(base_aleatoria_1))
 (median(base_aleatoria_2))
 
+# 2.e) Quartis
+(quantile(base_aleatoria_1))
+(quantile(base_aleatoria_2))
 
 
 
 #parei (Ferramentas em IA - Aula_17_11.pdf) página 21
-
 
 #########################################################
 ### teste +
@@ -64,16 +65,16 @@ funcao_media = function(base_aleatoria) {
 #library("ggplot2")
 
 
-sum(seq(1:5))
-
-hist(pnorm(1.96, 0, 1), 5, main = "Histograma da base aleatória 2, separado por faixas de 5", 
-     xlab = "Valores até 70", ylab = "Frequência")
-
-dados = qnorm(seq(1:5), 7.5, 1)
-hist(dados, 5, main = "Histograma da base aleatória 2, separado por faixas de 5", 
-     xlab = "Valores até 70", ylab = "Frequência", xlim = c(1, 5))
-
-pnorm(84, mean=72, sd=15.2, lower.tail=FALSE) 
+# sum(seq(1:5))
+# 
+# hist(pnorm(1.96, 0, 1), 5, main = "Histograma da base aleatória 2, separado por faixas de 5", 
+#      xlab = "Valores até 70", ylab = "Frequência")
+# 
+# dados = qnorm(seq(1:5), 7.5, 1)
+# hist(dados, 5, main = "Histograma da base aleatória 2, separado por faixas de 5", 
+#      xlab = "Valores até 70", ylab = "Frequência", xlim = c(1, 5))
+# 
+# pnorm(84, mean=72, sd=15.2, lower.tail=FALSE) 
 
 # https://www.r-bloggers.com/normal-distribution-functions/
 # http://www.r-tutor.com/elementary-statistics/probability-distributions/normal-distribution
@@ -82,24 +83,25 @@ pnorm(84, mean=72, sd=15.2, lower.tail=FALSE)
 #===== calculo manual da variancia
 
 #vetorx = c(1.00, 2.00, 3.00, 4.00, 5.00, 6.00, 7.00)
-vetorx = base_aleatoria_1
-
-sub_media_2 = function(x) {
-    
-  y = (x - as.numeric(mean(vetorx)))
-  return(y ^ 2) 
-  
-}
-
-soma_total = 0
-
-for(x in vetorx) {
-  print(x)
-  soma_total = soma_total + sub_media_2(x)
-}
-
-(soma_total/length(vetorx))
+# vetorx = base_aleatoria_1
+# 
+# sub_media_2 = function(x) {
+#     
+#   y = (x - as.numeric(mean(vetorx)))
+#   return(y ^ 2) 
+#   
+# }
+# 
+# soma_total = 0
+# 
+# for(x in vetorx) {
+#   print(x)
+#   soma_total = soma_total + sub_media_2(x)
+# }
+# 
+# (soma_total/length(vetorx))
 
 ### teste - 
 #########################################################
+
 
