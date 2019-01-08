cb3c743567ab395e04563a3adcd8af615bc4abd4
nubiofs <nubiofs@gmail.com>
2019-01-08 14:12:22 -0300

    vi

---
+++ Python-Linguagem/projetos/PycharmProjects/estudando_pythondrops/raiz2grau.py
@@ -0,0 +1,19 @@
+import math
+
+a = 1
+b = -5
+c = 6
+
+x = (b ** 2) - (4 * a * c)
+
+if x < 0:
+
+	print("Raiz negativa")
+
+else:
+
+	r = math.sqrt(x)
+	x1 = (-b + r) / (2 * a)
+	x2 = (-b - r) / (2*a)
+
+print("\n\nX' = %s \nX' = " % x1, x2)
