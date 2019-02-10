25ee892f247b194ed0a69796e5a8fb2dcf631cf4
nubiofs <nubiofs@gmail.com>
2019-02-10 11:25:21 -0300

    up

--- CienciasDosDados/pos-ver/AnaliseRedeSociais/trabalhos_finais/predicao/predicao_publicacao.py
+++ CienciasDosDados/pos-ver/AnaliseRedeSociais/trabalhos_finais/predicao/predicao_publicacao.py
@@ -3,6 +3,15 @@ import pandas as pd
 from datetime import datetime
 import networkx as nx
 import itertools
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+# Downloads necessarios, apenas uma vez, para os processos de Tokenize e Stop Words:
+#nltk.download('punkt')
+#nltk.download()
+# Use the NLTK Downloader to obtain the resource
+# Downloading package punkt to ~/nltk_data...
 
 def obter_autores_artigos_from_arxiv(from_arxiv):
 
@@ -131,8 +140,8 @@ def gerar_grafo(df_artigos):
         title = list(set(df['title']))
         summary = list(set(df['summary']))
         for a1, a2 in itertools.combinations(lista_autores, 2):
-            G.add_node(a1, summary_concat = list())
-            G.add_node(a2, summary_concat = list())
+            G.add_node(a1, summary_tokenize_stop_word = list())
+            G.add_node(a2, summary_tokenize_stop_word = list())
             G.add_edge(a1, a2, id_link = i, title = title[0], summary = summary[0], Weight = 3)        
 
     return G
@@ -147,59 +156,78 @@ print('\nInformações dos grafos (antes da adição das arresta de predição):
 print('- grafo 2016: \n', nx.info(grafo_2016))
 print('- grafo 2017: \n', nx.info(grafo_2017))
 
-def adicionar_summary_concat_para_autores(grafo, n1, n2):
+def adicionar_summary_tokenize_stop_word_para_autores(grafo, n1, n2):
 
     dd = grafo.get_edge_data(n1, n2)
     summary = dd.get('summary')
-    if not summary in grafo.node[n1]['summary_concat']:
-        grafo.node[n1]['summary_concat'].append(summary)
-
-    if not summary in grafo.node[n2]['summary_concat']:
-        grafo.node[n2]['summary_concat'].append(summary)
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(summary)
+    filtered_sentence = [w for w in tokens if (not w in stop_words) and (w.isalpha())]
+    for s in filtered_sentence:
+        if not s in grafo.node[n1]['summary_tokenize_stop_word']:
+            grafo.node[n1]['summary_tokenize_stop_word'].append(s)
+        if not s in grafo.node[n2]['summary_tokenize_stop_word']:
+            grafo.node[n2]['summary_tokenize_stop_word'].append(s)
 
 lista_autores_predicao_em_2016_para_2017 = []
 # Obter todos os nós 'autores' que, com base no valor '2' da distância entre eles, podem 
 # ser preditos em 2016 (ou seja, que podem ou não publicar em conjunto no ano de 2017).
-# Obs.: Também será adicionado nos nós 'autores' vizinhos o atributo 'summary_concat' da 
+# Obs.: Também será adicionado nos nós 'autores' vizinhos o atributo 'summary_tokenize_stop_word' da 
 # concatenação de todos os identificadores únicos dos artigos que o autor participa. 
 for n1 in grafo_2016.nodes:
     # Faz todo o rastreamento de vizinhos dos vizinhos a apartir do nó 'n1'
     for n2 in grafo_2016.neighbors(n1):
         assert(nx.shortest_path_length(grafo_2016, source=n1, target=n2) == 1)
-        adicionar_summary_concat_para_autores(grafo_2016, n1, n2)
+        adicionar_summary_tokenize_stop_word_para_autores(grafo_2016, n1, n2)
         for n3 in grafo_2016.neighbors(n2):
             assert(nx.shortest_path_length(grafo_2016, source=n2, target=n3) == 1)
-            adicionar_summary_concat_para_autores(grafo_2016, n2, n3)
+            adicionar_summary_tokenize_stop_word_para_autores(grafo_2016, n2, n3)
             if nx.shortest_path_length(grafo_2016, source=n1, target=n3) == 2:
                 if not [n1, n3] in lista_autores_predicao_em_2016_para_2017:
                     lista_autores_predicao_em_2016_para_2017.append([n1, n3]) #Uma lista de listas
 
 # Calculo para ver se predição "(sim - VP / FP) ou (não - VN / FN)":
 def calculo_coeficiente_predicao(n1, n2):
-    return True
+
+    def jaccard(a, b):
+        c = a.intersection(b)
+        return float(len(c)) / (len(a) + len(b) - len(c))
+
+    words1 = set(grafo_2016.node[n1]['summary_tokenize_stop_word'])
+    words2 = set(grafo_2016.node[n2]['summary_tokenize_stop_word'])
+
+    return jaccard(words1, words2)
 
 # Adiciona aresta para n1 e n2 (no grafo do ano 2016), apenas se os nós 'autor' 
 # tiverem publicado nos dois anos seguidos (2016 e 2017) e tiverem calculo de 
 # coeficiente predição suficiente:
 for n1, n2 in lista_autores_predicao_em_2016_para_2017:
+
     if n1 in autores_validos and n2 in autores_validos:
         
-        # if calculo_coeficiente_predicao(n1, n2):
-        
+        id_link = ''
+
+        if calculo_coeficiente_predicao(n1, n2) >= 0.1:
+            id_link = 'YES?'
+        else:
+            id_link = 'NO?'
+
         # Obs.:
-        # O valor '?' para o atributo id_link serve pra informar que é uma predição de publicação para 2017.
+        # O valor 'YES? / NO?' para o atributo id_link serve pra informar que é uma predição de publicação para 2017.
         # O valor '6' para o atributo Weight serve pra diferenciar no gráfico que é uma aresta de predição
-        grafo_2016.add_edge(n1, n2, id_link = '?', title = 'title', summary = 'summary', Weight = 6)
+        grafo_2016.add_edge(n1, n2, id_link = id_link, title = 'title', summary = 'summary', Weight = 6)
+
+
+# Calculo da matriz de confusão:
 
 print('\nInformações dos grafos (depois da adição das arresta de predição): \n')
 print('- grafo 2016: \n', nx.info(grafo_2016))
 print('- grafo 2017: \n', nx.info(grafo_2017))
 
 # export your data into Gephi’s GEXF format:
-
 for n, d in grafo_2016.nodes(data=True):
-    lista = grafo_2016.node[n]['summary_concat']
-    grafo_2016.node[n]['summary_concat'] = '[$-#-$]'.join(lista)
-
+    lista = grafo_2016.node[n]['summary_tokenize_stop_word']
+    #Transformar para uma concatenção das stop_word (pois o gephi não lê 'lista de lista')
+    grafo_2016.node[n]['summary_tokenize_stop_word'] = '[#]'.join(lista)
 nx.write_gexf(grafo_2016, 'grafo_2016_in_gephi.gexf')
 nx.write_gexf(grafo_2017, 'grafo_2017_in_gephi.gexf')
