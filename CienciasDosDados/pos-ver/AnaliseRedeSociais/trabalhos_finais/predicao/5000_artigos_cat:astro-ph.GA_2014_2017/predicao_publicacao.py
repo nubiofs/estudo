76874833a3a640f927c3440d0620197c9dc8a7d1
nubiofs <nubiofs@gmail.com>
2019-02-18 13:24:54 -0300

    vi

--- CienciasDosDados/pos-ver/AnaliseRedeSociais/trabalhos_finais/predicao/5000_artigos_cat:astro-ph.GA_2014_2017/predicao_publicacao.py
+++ CienciasDosDados/pos-ver/AnaliseRedeSociais/trabalhos_finais/predicao/5000_artigos_cat:astro-ph.GA_2014_2017/predicao_publicacao.py
@@ -16,7 +16,7 @@ from nltk.corpus import stopwords
 def obter_autores_artigos_from_arxiv(from_arxiv):
 
     df_autor_artigos_2014 = []
-    df_autor_artigos_2016 = []
+    df_autor_artigos_2017 = []
 
     if from_arxiv:
 
@@ -24,7 +24,7 @@ def obter_autores_artigos_from_arxiv(from_arxiv):
         print('\nQuantidades de artigos baixados: ', len(list(results)))
 
         artigos_2014 = []
-        artigos_2016 = []
+        artigos_2017 = []
         for r in results:
             autores = r.get('authors')
             if len(list(autores)) > 1:
@@ -39,8 +39,8 @@ def obter_autores_artigos_from_arxiv(from_arxiv):
                         title, 
                         summary,
                         data_published.year))
-                elif data_published.year == 2016:
-                    artigos_2016.append((
+                elif data_published.year == 2017:
+                    artigos_2017.append((
                         autores,
                         id, 
                         title, 
@@ -48,7 +48,7 @@ def obter_autores_artigos_from_arxiv(from_arxiv):
                         data_published.year))
 
         print('\nQuantidades de artigos filtrados, com mais de um autor, para o ano de 2014 = ', len(artigos_2014))
-        print('Quantidades de artigos filtrados, com mais de um autor, para o ano de 2016 = ', len(artigos_2016))
+        print('Quantidades de artigos filtrados, com mais de um autor, para o ano de 2017 = ', len(artigos_2017))
 
         # Gerar dataFrame para todos os registros de um dado artigo e seus respectivos autores
         def gerar_dataframe_autor_artigos(artigos):
@@ -77,10 +77,10 @@ def obter_autores_artigos_from_arxiv(from_arxiv):
             columns=['author', 'id', 'title', 'summary', 'published_year'], 
             encoding='utf-8', index=False, sep='@')
 
-        df_autor_artigos_2016 = gerar_dataframe_autor_artigos(artigos_2016)
-        print('df_autor_artigos_2016.shape: ', df_autor_artigos_2016.shape)
-        df_autor_artigos_2016.to_csv(
-            'registros_autores_artigos_2016_cat:astro-ph.GA.csv',
+        df_autor_artigos_2017 = gerar_dataframe_autor_artigos(artigos_2017)
+        print('df_autor_artigos_2017.shape: ', df_autor_artigos_2017.shape)
+        df_autor_artigos_2017.to_csv(
+            'registros_autores_artigos_2017_cat:astro-ph.GA.csv',
             columns=['author', 'id', 'title', 'summary', 'published_year'], 
             encoding='utf-8', index=False, sep='@')
 
@@ -90,11 +90,11 @@ def obter_autores_artigos_from_arxiv(from_arxiv):
             'registros_autores_artigos_2014_cat:astro-ph.GA.csv',
             encoding='utf-8', sep='@')
 
-        df_autor_artigos_2016 = pd.read_csv(
-            'registros_autores_artigos_2016_cat:astro-ph.GA.csv',
+        df_autor_artigos_2017 = pd.read_csv(
+            'registros_autores_artigos_2017_cat:astro-ph.GA.csv',
             encoding='utf-8', sep='@')
 
-    return df_autor_artigos_2014, df_autor_artigos_2016 
+    return df_autor_artigos_2014, df_autor_artigos_2017 
 
 #
 # Construindo o grafo não direcionado com nós para CADA AUTOR dos artigos.
@@ -121,11 +121,11 @@ def gerar_grafo(df_artigos):
 
     return G
 
-df_2014, df_2016 = obter_autores_artigos_from_arxiv(True)
-#df_2014, df_2016 = obter_autores_artigos_from_arxiv(False)
+df_2014, df_2017 = obter_autores_artigos_from_arxiv(True)
+#df_2014, df_2017 = obter_autores_artigos_from_arxiv(False)
 
 grafo_2014 = gerar_grafo(df_2014)
-grafo_2016 = gerar_grafo(df_2016)
+grafo_2017 = gerar_grafo(df_2017)
 
 print('\nInformações do grafo (antes da adição das arresta de predição): ')
 print('- grafo 2014: \n', nx.info(grafo_2014))
@@ -145,25 +145,25 @@ def adicionar_summary_tokenize_stop_word_para_autores(grafo, n1, n2):
 
 #
 # Obter todos os nós 'autores' que, com base no valor '2' da distância entre eles, podem 
-# ser preditos em 2014 (ou seja, que podem ou não publicar em conjunto no ano de 2016).
+# ser preditos em 2014 (ou seja, que podem ou não publicar em conjunto no ano de 2017).
 # Obs.: Também será adicionado nos nós 'autores' vizinhos o atributo 'summary_tokenize_stop_word' da 
 # concatenação de todos os identificadores únicos dos artigos que o autor participa. 
 #
-lista_autores_predicao_em_2014_para_2016 = []
+lista_autores_predicao_em_2014_para_2017 = []
 for n1 in grafo_2014.nodes:
     # Obter apenas autores que tenham alguma publicação nos dois anos
-    if grafo_2016.has_node(n1): 
+    if grafo_2017.has_node(n1): 
         # Faz todo o rastreamento de vizinhos dos vizinhos a apartir do nó 'n1'
         for n2 in grafo_2014.neighbors(n1):
             assert(nx.shortest_path_length(grafo_2014, source=n1, target=n2) == 1)
             adicionar_summary_tokenize_stop_word_para_autores(grafo_2014, n1, n2)
             for n3 in grafo_2014.neighbors(n2):
                 # Obter apenas autores que tenham alguma publicação nos dois anos
-                if grafo_2016.has_node(n3): 
+                if grafo_2017.has_node(n3): 
                     assert(nx.shortest_path_length(grafo_2014, source=n2, target=n3) == 1)
                     adicionar_summary_tokenize_stop_word_para_autores(grafo_2014, n2, n3)
-                    if (nx.shortest_path_length(grafo_2014, source=n1, target=n3) == 2) and (not [n1, n3] in lista_autores_predicao_em_2014_para_2016):
-                        lista_autores_predicao_em_2014_para_2016.append([n1, n3]) #Uma lista de listas
+                    if (nx.shortest_path_length(grafo_2014, source=n1, target=n3) == 2) and (not [n1, n3] in lista_autores_predicao_em_2014_para_2017):
+                        lista_autores_predicao_em_2014_para_2017.append([n1, n3]) #Uma lista de listas
 
 #
 # Calculo para ver se predição "(sim - VP / FP) ou (não - VN / FN)":
@@ -181,10 +181,10 @@ def calculo_coeficiente_predicao(n1, n2):
 
 #
 # Adiciona aresta para n1 e n2 (no grafo do ano 2014), apenas se os nós 'autor' 
-# tiverem publicado nos dois anos seguidos (2014 e 2016) e tiverem calculo de 
+# tiverem publicado nos dois anos seguidos (2014 e 2017) e tiverem calculo de 
 # coeficiente predição suficiente:
 #
-for n1, n2 in lista_autores_predicao_em_2014_para_2016:
+for n1, n2 in lista_autores_predicao_em_2014_para_2017:
 
     id_link = ''
 
@@ -194,7 +194,7 @@ for n1, n2 in lista_autores_predicao_em_2014_para_2016:
         id_link = 'NO?'
 
     # Obs.:
-    # O valor 'YES? / NO?' para o atributo id_link serve pra informar que é uma predição de publicação para 2016.
+    # O valor 'YES? / NO?' para o atributo id_link serve pra informar que é uma predição de publicação para 2017.
     # O valor '6' para o atributo Weight serve pra diferenciar no gráfico que é uma aresta de predição
     grafo_2014.add_edge(n1, n2, id_link = id_link, title = 'title', summary = 'summary', Weight = 6)
 
@@ -213,7 +213,7 @@ print('\tQuantidade arestas predição YES para 2014: ', quantidade_arestas_pred
 quantidade_arestas_predicao_yes_2014_acertos = 0
 quantidade_arestas_predicao_yes_2014_erros = 0
 for u, v in lista_aresta_predicao_yes_2014:
-    if grafo_2016.has_edge(u, v):
+    if grafo_2017.has_edge(u, v):
         quantidade_arestas_predicao_yes_2014_acertos = quantidade_arestas_predicao_yes_2014_acertos + 1
     else:
         quantidade_arestas_predicao_yes_2014_erros = quantidade_arestas_predicao_yes_2014_erros + 1
@@ -230,7 +230,7 @@ print('\n\tQuantidade arestas predição NO para 2014: ', quantidade_arestas_pre
 quantidade_arestas_predicao_no_2014_acertos = 0
 quantidade_arestas_predicao_no_2014_erros = 0
 for u, v in lista_aresta_predicao_no_2014:
-    if not grafo_2016.has_edge(u, v):
+    if not grafo_2017.has_edge(u, v):
         quantidade_arestas_predicao_no_2014_acertos = quantidade_arestas_predicao_no_2014_acertos + 1
     else:
         quantidade_arestas_predicao_no_2014_erros = quantidade_arestas_predicao_no_2014_erros + 1
@@ -254,4 +254,4 @@ for n, d in grafo_2014.nodes(data=True):
     #Transformar para uma concatenção das stop_word (pois o gephi não lê 'lista de lista')
     grafo_2014.node[n]['summary_tokenize_stop_word'] = '[#]'.join(lista)
 nx.write_gexf(grafo_2014, 'grafo_2014_in_gephi.gexf')
-nx.write_gexf(grafo_2016, 'grafo_2016_in_gephi.gexf')
+nx.write_gexf(grafo_2017, 'grafo_2017_in_gephi.gexf')
