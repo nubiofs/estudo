743f4b20cd7d2a8d571f5c101e926aceb124723f
nubiofs <nubiofs@gmail.com>
2019-08-14 12:08:05 -0300

    viPr

---
+++ prometheus/TowerOfHanoi/src/main/java/com/example/TowerOfHanoi/TowerOfHanoi.java
@@ -0,0 +1,34 @@
+package com.example.TowerOfHanoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Java recursive program to solve tower of hanoi puzzle 
+//https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/
+
+@SpringBootApplication
+public class TowerOfHanoi {
+
+	// Java recursive function to solve tower of hanoi puzzle 
+	static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) 
+	{ 
+		if (n == 1) 
+		{ 
+			System.out.println("Move disk 1 from rod " + from_rod + " to rod " + to_rod); 
+			return; 
+		} 
+		towerOfHanoi(n-1, from_rod, aux_rod, to_rod); 
+		System.out.println("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod); 
+		towerOfHanoi(n-1, aux_rod, to_rod, from_rod); 
+	} 
+	
+	// Driver method 
+	public static void main(String args[]) 
+	{ 
+		int n = 4; // Number of disks 
+		towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
+		while(true);
+ 
+	}
+ 
+}
---
+++ prometheus/TowerOfHanoi/src/test/java/com/example/TowerOfHanoi/TowerOfHanoiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.TowerOfHanoi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TowerOfHanoiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
